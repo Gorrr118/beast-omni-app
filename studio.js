46e96cc9160d87c805dd01adf66985c6cb7349bf
Gorrr118 <gorpro52@gmail.com>
2026-09-21 23:04:46 +0400

    Update studio.js

--- studio.js
+++ studio.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('DOMContentLoaded', () => {
     // === Инициализация базовых элементов плеера и загрузки ===
     const playerScreenTrigger = document.getElementById('player-screen-trigger');
     const videoUpload = document.getElementById('video-upload');
@@ -480,4 +481,5 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
+});
 });
